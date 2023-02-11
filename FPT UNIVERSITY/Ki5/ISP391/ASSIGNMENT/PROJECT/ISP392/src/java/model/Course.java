14f3dae420be2fce7e13fdcd6b7330c2e9200657
Thangvxhe161552 <thangvxhe161552@fpt.edu.vn>
2023-02-11 23:43:25 +0700

    Course controller,DAO

--- FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/model/Course.java
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/model/Course.java
@@ -8,6 +8,22 @@ package model;
  *
  * @author vuxua
  */
+import java.sql.Date;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
 public class Course {
-    
+
+    private int id;
+    private String thumnailURL;
+    private String content;
+    private String description;
+    private Date createDate;
+    private int IDcategory;
 }
