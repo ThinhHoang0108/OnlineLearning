c3b9903bee6d22427cc78130529ba54f52f52e20
NuocCodeDua <thinhhxhe161617@fpt.edu.vn>
2023-02-20 10:49:47 +0700

    register

--- FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/quiz/AddNewQuizz.java
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/quiz/AddNewQuizz.java
@@ -29,16 +29,7 @@ public class AddNewQuizz extends HttpServlet {
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AddNewQuizz</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AddNewQuizz at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+          
         }
     } 
 
