23917b49d77c5f13e951bf43c4f6e5e7f27cbd91
NuocCodeDua <thinhhxhe161617@fpt.edu.vn>
2023-02-20 10:50:36 +0700

    register

--- FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/quiz/QuizDetailController.java
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/quiz/QuizDetailController.java
@@ -29,16 +29,7 @@ public class QuizDetailController extends HttpServlet {
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet QuizDetailController</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet QuizDetailController at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            
         }
     } 
 
