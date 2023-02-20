fdcd2fda1fbaa5ae64c26e27be2283af42c0c70b
NuocCodeDua <thinhhxhe161617@fpt.edu.vn>
2023-02-20 10:50:14 +0700

    register

--- FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/quiz/EditQuizController.java
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/quiz/EditQuizController.java
@@ -29,16 +29,7 @@ public class EditQuizController extends HttpServlet {
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EditQuizController</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EditQuizController at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           
         }
     } 
 
