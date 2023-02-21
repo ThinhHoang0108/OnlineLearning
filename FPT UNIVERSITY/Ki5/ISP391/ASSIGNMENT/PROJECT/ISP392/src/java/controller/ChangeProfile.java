03bfe3718ca7b228d1382361c390a5bd2dbe69a3
dell <lythhe161708@fpt.edu.vn>
2023-02-22 01:03:00 +0700

    Change profile

---
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/ChangeProfile.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import DAO.UserDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import model.User;
+
+/**
+ *
+ * @author SHD
+ */
+public class ChangeProfile extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        User u = (User)request.getSession().getAttribute("account");
+        request.setAttribute("user", u);
+        request.getRequestDispatcher("changeProfile.jsp").forward(request, response);  
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String dob = request.getParameter("dob");
+        String phoneNumber = request.getParameter("phone");
+        String password = request.getParameter("pass");
+        int id = Integer.parseInt(request.getParameter("id"));
+        new UserDAO().updateUser(dob, phoneNumber, password,id);
+        request.setAttribute("msg", "Success");
+        request.setAttribute("id", id);
+        request.getRequestDispatcher("changeProfile.jsp").forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
