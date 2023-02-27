43a990f8d69f91f4a37944fecc23ce4f3cfd127d
Thangvxhe161552 <thangvxhe161552@fpt.edu.vn>
2023-02-27 14:37:54 +0700

    add course

--- FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/Course/addCourse.java
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/Course/addCourse.java
@@ -2,49 +2,56 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.Course;
 
+import DAO.CourseDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import model.Course;
 
 /**
  *
  * @author vuxua
  */
+@WebServlet(name = "addCourse", urlPatterns = {"/addcourse"})
 public class addCourse extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet addCourse</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet addCourse at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try ( PrintWriter out = response.getWriter()) {
+            String thumnail_raw = request.getParameter("thumnail");
+            String content_raw = request.getParameter("content");
+            String description_raw = request.getParameter("description");
+            String datecreate_raw = request.getParameter("datecreate");
+            String category_raw = request.getParameter("category");
+            DAO.CourseDAO dao = new CourseDAO();
+            dao.insertCourse(content_raw, thumnail_raw, description_raw, datecreate_raw, category_raw);
+           // request.getRequestDispatcher("editcourse.jsp").forward(request, response);
+            response.sendRedirect("editcourse");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -52,12 +59,13 @@ public class addCourse extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -65,12 +73,13 @@ public class addCourse extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
