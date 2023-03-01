ae61f508101f6ce1d9900d6300dd18fd43b355d1
Thangvxhe161552 <thangvxhe161552@fpt.edu.vn>
2023-03-01 15:33:47 +0700

    Update course detail

--- FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/Course/CourseDetail.java
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/Course/CourseDetail.java
@@ -2,48 +2,58 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.Course;
 
 import DAO.CourseDAO;
+import DAO.SubjectDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import model.Course;
+import model.Subject;
 
 /**
  *
  * @author vuxua
  */
+@WebServlet(name = "CourseDetail", urlPatterns = {"/coursedetail"})
 public class CourseDetail extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             int courseID = Integer.parseInt(request.getParameter("courseID"));
             //Course course = new CourseDAO;
             String xid = request.getParameter("id");
             DAO.CourseDAO dao = new CourseDAO();
             Course course = dao.getCourseById(courseID);
             request.setAttribute("course", course);
+            SubjectDAO dao1 = new SubjectDAO();
+            List<Subject> listSubject = dao1.getAllSubject(courseID);
+            request.setAttribute("listSubject", listSubject);
             request.getRequestDispatcher("coursedetail.jsp").forward(request, response);
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -51,12 +61,13 @@ public class CourseDetail extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -64,12 +75,13 @@ public class CourseDetail extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
