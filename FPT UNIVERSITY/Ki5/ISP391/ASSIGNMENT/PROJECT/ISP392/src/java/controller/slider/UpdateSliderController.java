807f53726df3345b82d476053a846a38823bc061
NMT1510 <93635950+NMT1510@users.noreply.github.com>
2023-03-11 16:04:16 +0700

    slider

---
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/slider/UpdateSliderController.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.slider;
+
+import DAO.SliderDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ADMIN
+ */
+@WebServlet(name="UpdateSliderController", urlPatterns={"/updateSlider"})
+public class UpdateSliderController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UpdateSliderController</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateSliderController at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String id_raw = request.getParameter("id");
+        String sliderUrl_raw = request.getParameter("sliderUrl");
+        String title_raw = request.getParameter("title");
+        String content_raw = request.getParameter("content");
+        String notes_raw = request.getParameter("notes");
+        SliderDAO s = new SliderDAO();
+        s.updateSlider(title_raw, true, id_raw, content_raw, notes_raw, 0);
+        request.getRequestDispatcher("sliderList").forward(request, response);
+        
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/model/Slider.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author ADMIN
+ */
+@Builder
+@Getter
+@Setter
+@ToString
+public class Slider {
+     private int sliderId;
+     private String slider_url;
+     private boolean status;
+     private String title;
+     private String content;
+     private String backlink;
+     private String notes;
+     private boolean isShow;
+     private int courseId;
+}
