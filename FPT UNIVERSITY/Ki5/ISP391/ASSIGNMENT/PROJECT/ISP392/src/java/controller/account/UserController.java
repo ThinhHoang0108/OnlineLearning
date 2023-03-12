be328f8b6aa7ad5beb872e267f1ae835c7f97c57
dell <lythhe161708@fpt.edu.vn>
2023-03-13 03:27:58 +0700

    Change profile

--- FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/account/UserController.java
+++ FPT UNIVERSITY/Ki5/ISP391/ASSIGNMENT/PROJECT/ISP392/src/java/controller/account/UserController.java
@@ -33,7 +33,7 @@ public class UserController extends HttpServlet {
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException if an I/O error occursdâsdasdasdasda
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
